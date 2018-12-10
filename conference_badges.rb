4b4c5c74110cd25f613223fc885d17f4d15e1362
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-12-10 02:39:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -15,3 +16,16 @@ def printer(attendees)
   batch_badge_creator(attendees).each {|name| puts name}
   assign_rooms(attendees).each {|speaker| puts speaker}
 end
+=======
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is " + name
+end
+
+def batch_badge_creator(names)
+  names.map! {|name| badge_maker(name)}
+end
+
+batch_badge_creator(names)
+>>>>>>> 3eb61d7d59442abdb6c6047a5fb3c71c1e143dd2
